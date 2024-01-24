cd41b2f8664608a019c503d3288f89ba2786a855
Victor JEMAN <victorjeman@gmail.com>
2024-01-24 09:29:34 +0200

    Changes for dummy components start point

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
-import { ChatControls } from './components/chat-controls'
-import { ChatDiscussionList } from './components/chat-discussion-list'
-import { ChatMessageList } from './components/chat-message-list'
-import { ChatStartDiscussionModal } from './components/chat-start-discussion-modal'
+import { ChatContact } from './components/chat-contact'
 
 export default function App() {
   return (
     <div>
-      <ChatStartDiscussionModal />
-      <ChatControls />
-      <ChatDiscussionList />
-      <ChatMessageList />
+      <ChatContact />
     </div>
   )
 }
