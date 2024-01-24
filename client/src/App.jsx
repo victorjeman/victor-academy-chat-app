a7a6f1a92d49bf547b9f8ae4177e48785da8a9e8
Victor JEMAN <victorjeman@gmail.com>
2024-01-24 10:05:05 +0200

    Define the app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import { ChatControls } from './components/chat-controls'
 import { ChatDiscussionList } from './components/chat-discussion-list'
+import { ChatLayout } from './components/chat-layout'
 import { ChatMessageList } from './components/chat-message-list'
 import { ChatStartDiscussionModal } from './components/chat-start-discussion-modal'
 
+import './App.css'
+
 export default function App() {
   return (
-    <div>
-      <ChatStartDiscussionModal />
-      <ChatControls />
-      <ChatDiscussionList />
-      <ChatMessageList />
-    </div>
+    <>
+      {false && <ChatStartDiscussionModal />}
+
+      <ChatLayout
+        controls={<ChatControls />}
+        aside={<ChatDiscussionList />}
+        main={<ChatMessageList />}
+      />
+    </>
   )
 }
