526a1d7e7a6b62a0b1def7d3c247b3f460354092
Victor JEMAN <victorjeman@gmail.com>
2024-02-02 00:08:17 +0200

    step-use-clsx-final updates

--- client/src/components/chat-message.jsx
+++ client/src/components/chat-message.jsx
@@ -1,12 +1,3 @@
-/*
- * 1. Voi primi prin props un messagj care fi un obiect.
- * 2. Voi afisa continutul mesajului.
- * 3. Voi afisa data la care a fost trimis mesajul.
- * 4. Voi afisa autorul mesajului.
- * 5. As putea sa am o valoare in props care sa-mi spuna daca mesajul este al meu sau nu.
- * 6. Daca mesajul este al meu atunci voi afisa mesajul intr-un mod diferit.
- */
-
 export function ChatMessage({ message }) {
   return (
     <div>
