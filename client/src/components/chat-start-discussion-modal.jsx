eab90875d6671aa69274964c063e8b046361e7eb
Victor JEMAN <victorjeman@gmail.com>
2024-02-01 23:06:14 +0200

    step-show-modal-final updates

--- client/src/components/chat-start-discussion-modal.jsx
+++ client/src/components/chat-start-discussion-modal.jsx
@@ -1,13 +1,6 @@
 import { ChatContactList } from './chat-contact-list'
 import { ChatStartDiscussionButton } from './chat-start-discussion-button'
 
-/*
- * 1. Voi primi lista de contacte in props.
- * 2. Voi afisa lista de contacte folosind componenta ChatContactList.
- * 3. Voi primi functia ce trebuie apelata cand apas pe butonul "Start discussion".
- * 4. Voi transmite mai departe functia catre componenta ChatStartDiscussionButton.
- */
-
 export function ChatStartDiscussionModal() {
   return (
     <div>
