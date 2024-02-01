bc3cd065f64af72fa8afcd3b1b24eb53db1ab09f
Victor JEMAN <victorjeman@gmail.com>
2024-02-01 22:58:43 +0200

    step-layout-final updates

--- client/src/components/chat-controls.jsx
+++ client/src/components/chat-controls.jsx
@@ -1,8 +1,3 @@
-/*
- * 1. Aici voi un button care va afisa lista de contacte intr-un modal.
- * 2. Pe viitor ar putea fi alte butoane cu actiuuni diferite.
- */
-
 export function ChatControls() {
   return (
     <div>
