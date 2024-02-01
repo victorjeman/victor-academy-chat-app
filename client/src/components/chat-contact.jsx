050b0d0b049c085b6a6d9d4f32531d00ae08c30f
Victor JEMAN <victorjeman@gmail.com>
2024-02-01 23:11:05 +0200

    step-style-modal-final updates

--- client/src/components/chat-contact.jsx
+++ client/src/components/chat-contact.jsx
@@ -1,9 +1,3 @@
-/*
- * 1. O sa primesc un contact prin props.
- * 2. Fiecare contact va avea un nume si un id.
- * 3. Voi extrage numele din contact si-l voi afisa.
- */
-
 export function ChatContact() {
   return <div>ChatContact</div>
 }
